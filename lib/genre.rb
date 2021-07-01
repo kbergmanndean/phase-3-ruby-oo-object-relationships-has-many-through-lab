5cc58af90f13bb6cbc7fd78c4d64e2ebbcbfbf12
Kaia <kbergmanndean@gmail.com>
2021-07-01 15:08:13 -0400

    commit tree c1d175a499d58e837b6cc75a09096303539a8ab7

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,17 @@
-
+class Genre
+    @@all=[]
+    def self.all
+        @@all
+    end
+    attr_accessor :name
+    def initialize(name)
+        @name=name
+        @@all<<self
+    end
+    def songs
+        Song.all.select{|song|song.genre==self}
+    end
+    def artists
+        songs.map {|song| song.artist}.flatten
+    end
+end
--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,16 @@
+class Song
+    @@all=[]
+    def self.all
+        @@all
+    end
 
+    attr_accessor :name, :artist, :genre
+    def initialize(name,artist,genre)
+        @name=name
+        @artist=artist
+        @genre=genre
+        @@all<<self
+    end
+
+
+end
